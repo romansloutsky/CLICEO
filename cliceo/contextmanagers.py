a011528b9a5e97dbadaed6f81c1300de876be97a
Roman Sloutsky <devnull@localhost>
2017-05-15 14:16:33 -0500

    Context manager creates temporary file with desired contents and cleans it up

---
+++ cliceo/contextmanagers.py
@@ -0,0 +1,26 @@
+import tempfile
+import contextlib2
+
+@contextlib2.contextmanager
+def NamedTemporaryFileWithContents(contents,dirpath=None,bufsize=-1,mode='w+b',
+                                   suffix="",prefix=tempfile.template):
+  '''
+  Temporary file holding contents expected to be found in a file by a third
+  party, e.g. a CLI application.
+  
+  Argument contents may be a string to be written to the file or a callable that
+  takes a single argument, the file handle, and does the writing itself.
+  '''
+  try:
+    with tempfile.NamedTemporaryFile(mode=mode,bufsize=bufsize,suffix=suffix,
+                                     prefix=prefix,dir=dirpath,delete=False)\
+                                                               as tempfile_obj:
+      if callable(contents):
+        contents(tempfile_obj.file)
+      else:
+        tempfile_obj.file.write(contents)
+    yield tempfile_obj.name
+  finally:
+    tempfile_obj.unlink(tempfile_obj.name)
+
+
