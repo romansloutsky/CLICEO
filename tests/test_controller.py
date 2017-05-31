6bd47dd328e9ec7df2b21f90f2852ac58488a560
Roman Sloutsky <devnull@localhost>
2017-05-31 17:58:42 -0500

    Testing regular and temporary working directory handling

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -53,3 +53,40 @@ class test_CLIcontrollerBase_std_stream_handling(unittest.TestCase):
     patched_Popen.assert_called_with('ls',stdout=subprocess.PIPE,stderr=mockfh,
                                      shell=True)
     mockfh.__exit__.assert_called_once_with(mockfh,None,None,None)
+
+
+class test_CLIcontrollerBase_working_directory(unittest.TestCase):
+  
+  def test_regular_working_directory(self):
+    dummycontroller = controller.CLIcontrollerBase(callargs=['ls'])
+    self.assertEqual(dummycontroller.in_workdir('dummy'),
+                     os.path.join('.','dummy'))
+    
+    dummycontroller = controller.CLIcontrollerBase(dirpath='different_dir',
+                                                   callargs=['ls'])
+    self.assertEqual(dummycontroller.in_workdir('dummy'),
+                     os.path.join('different_dir','dummy'))
+  
+  @patch('subprocess.Popen')
+  @patch('procCEO.tempdir.TemporaryWorkingDirectory')
+  def test_temporary_working_directory(self,patched_TmpWorkDir,patched_Popen):
+    mockTmpWorkDir_obj = patched_TmpWorkDir.return_value 
+    mockTmpWorkDir_obj.__enter__.return_value = 'created_temporary_dir'
+    dummycontroller = controller.CLIcontrollerBase(dirpath='different_dir',
+                                                   in_tmpdir=True,
+                                                   callargs=['ls'])
+    self.assertIs(dummycontroller.tmpdir,True)
+    self.assertEqual(dummycontroller.dir,'different_dir')
+    dummycontroller()
+    patched_TmpWorkDir.assert_called_once_with(dir='different_dir',
+                                               prefix='tmp',suffix='')
+    mockTmpWorkDir_obj.__enter__.assert_called_once_with(mockTmpWorkDir_obj)
+    self.assertIs(dummycontroller.tmpdir,
+                  mockTmpWorkDir_obj.__enter__.return_value)
+    self.assertEqual(dummycontroller.dir,'.')
+    self.assertEqual(dummycontroller.in_workdir('dummy'),
+                     os.path.join('.','dummy'))
+    mockTmpWorkDir_obj.__exit__.assert_called_once_with(mockTmpWorkDir_obj,
+                                                        None,None,None)
+
+
