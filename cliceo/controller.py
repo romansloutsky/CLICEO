d4748384be3c55b08ef4be348351a6720d45b126
Roman Sloutsky <sloutsky@wustl.edu>
2018-07-13 23:03:32 -0400

    Detailed docstring for class CommandLineCaller

--- cliceo/controller.py
+++ cliceo/controller.py
@@ -5,6 +5,46 @@ from . import contextmanagers
 
 
 class CommandLineCaller(object):
+  '''
+  Intended to be used as base class for concrete command line controller classes
+  for specific command line programs. Provides management of CLI exectution
+  context via CLIcontextManager.
+  
+  Initialization parameters:
+    :param callstr: String to be passed to subprocess.Popen() to be executed at
+                    the command line
+                    No default value -- a call string must be provided at
+                    initialization
+    :param PIDpublisher: Callable for reporting the PID of launched process
+    
+    Temporary working directory control:
+      :param in_tmpdir: Boolean flag indicating whether a TemporaryWorkingDirectory
+                        should be created for the duration of execution
+      :param tmpdir_loc: Location where temporary directory should be created
+                         If None, directory is created at the temporary location
+                         specified by the OS
+                         Ignored if in_tmpdir evaluates to False
+    
+    STDOUT/STDERR control:
+      :param capture_stdout: Boolean flag indicating whether the STDOUT output
+                             of created process should be captured
+                             If True, output is stored as attribute
+                             'captured_stdout'
+      :param silence_stdout: Boolean flag indicating whether the STDOUT output
+                             of created process should be redirected to os.devnull
+                             Ignored if capture_stdout evaluates to True
+      :param err_to_out: Boolean flag indicating whether STDERR output of created
+                         process should be redirected to STDOUT (2>&1)
+      :param capture_stderr: Boolean flag indicating whether the STDERR output
+                             of created process should be captured
+                             If True, output is stored as attribute
+                             'captured_stderr'
+                             Ignored if err_to_out evaluates to True
+      :param silence_stderr: Boolean flag indicating whether the STDERR output
+                             of created process should be redirected to os.devnull
+                             Ignored if either err_to_out or capture_stdout
+                             evaluate to True
+  '''
   
   @classmethod
   def get_CLI_context_manager(cls):
